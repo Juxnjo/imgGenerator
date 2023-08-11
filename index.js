66ccb647dd91cf34b55238d83bbe1f8d632be625
“Juan <ramirezjuanj0117@gmail.com>
2023-08-11 00:34:12 -0500

    OK

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const OPENAI_API_KEY = "";
 
 const btn = document.querySelector( "#btn" );
 const input = document.querySelector( "#input" );
-const img = document.querySelector("#img")
+const img = document.querySelector( "#img" )
 
 btn.addEventListener( "click", async () =>
 {
-    console.log( input.value );
+    if ( input.value === '' )
+    {
+        alert( 'Ingresa una peticion ' )
+        return
+    }
+
+    btn.disabled = true
 
     const res = await fetch( "https://api.openai.com/v1/images/generations", {
         method: "POST",
         body: JSON.stringify( {
-            prompt: "Un perro",
+            prompt: input.value,
             n: 1,
             size: "1024x1024",
         } ),
@@ -23,5 +29,10 @@ btn.addEventListener( "click", async () =>
     } );
 
     const data = await res.json();
-    console.log( data );
+
+    console.log( data.data[ 0 ].url );
+
+    img.src = data.data[ 0 ].url
+    
+    btn.disabled = false
 } )
